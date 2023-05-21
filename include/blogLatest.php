f67ab79e740055d38dd4c86c3e6a7083a8815b37
jo0132 <noji70770@gmail.com>
2023-05-21 09:38:08 +0900

    2023.05.21
    1111

--- include/blogLatest.php
+++ include/blogLatest.php
@@ -1,12 +1,12 @@
 <div class="cate">
     <h4>최신 글</h4>
     <ul>
-    <?php
+<?php
     $sql = "SELECT * FROM blog WHERE blogDelete = 0 ORDER BY blogID DESC LIMIT 5";
         $result = $connect -> query($sql);
-    ?>
-    <?php foreach($result as $blog){ ?>
-<li><a href="blogview.php?blogID=<?=$blog['blogID']?>"><?=$blog['blogTitle']?></a></li>
-    <?php } ?>    
-</ul>
+?>
+<?php foreach($result as $blog){ ?>
+    <li><a href="blogview.php?blogID=<?=$blog['blogID']?>"><?=$blog['blogTitle']?></a></li>
+<?php } ?>    
+    </ul>
 </div>
--- include/blogPopular.php
+++ include/blogPopular.php
@@ -1,3 +1,12 @@
 <div class="cate">
     <h4>인기 글</h4>
+    <ul>
+<?php
+    $sql = "SELECT * FROM blog WHERE blogDelete = 0 ORDER BY blogView DESC LIMIT 5";
+        $result = $connect -> query($sql);
+?>
+<?php foreach($result as $blog){ ?>
+    <li><a href="blogview.php?blogID=<?=$blog['blogID']?>"><?=$blog['blogTitle']?></a></li>
+<?php } ?>    
+    </ul>
 </div>
